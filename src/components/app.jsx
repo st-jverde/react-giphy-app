6e76e98c691b743ddf302fd944a9e8f789d57bca
Joeri Verdegaal <joeriverdegaal@gmail.com>
2022-12-14 16:06:39 +0100

    react app start

---
+++ src/components/app.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene"></div>
+        <div className="right-scene"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
+import App from './components/app';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(
+    <App />,
+    root
+  );
 }
+
+// const Hello = ({ name }) => {
+//   return (
+//     <div>
+//       Hello,
+//       {name}
+//     </div>
+//   );
+// };
